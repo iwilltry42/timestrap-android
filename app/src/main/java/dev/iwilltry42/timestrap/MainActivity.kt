5880d8d96c0ce6647932ad0f194212a04baec81c
Thorsten Klein <iwilltry42@gmail.com>
2020-07-06 14:12:13 +0200

    refactoring

--- app/src/main/java/dev/iwilltry42/timestrap/MainActivity.kt
+++ app/src/main/java/dev/iwilltry42/timestrap/MainActivity.kt
@@ -2,21 +2,10 @@ package dev.iwilltry42.timestrap
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.text.Editable
-import android.text.TextWatcher
-import android.util.Base64
-import android.util.Log
 import android.widget.*
-import com.android.volley.Request
-import com.android.volley.Response
-import com.android.volley.toolbox.JsonObjectRequest
-import com.android.volley.toolbox.Volley
-import com.google.gson.Gson
 import com.google.gson.GsonBuilder
 import dev.iwilltry42.timestrap.tasks.TaskContent
-import kotlinx.android.synthetic.main.activity_login.*
 import kotlinx.android.synthetic.main.activity_main.*
-import org.json.JSONObject
 
 
 class MainActivity : AppCompatActivity() {
@@ -31,11 +20,11 @@ class MainActivity : AppCompatActivity() {
         }
 
         main_button.setOnClickListener {
-            getEntries()
+            fetchTasks()
         }
     }
 
-    private fun getEntries() {
+    private fun fetchTasks() {
         requestAPIArrayWithTokenAuth(this, AppPreferences.address, "/api/tasks/", AppPreferences.token) {success, response ->
             val responseText = findViewById<TextView>(R.id.response_text)
             if (success && response != null) {
