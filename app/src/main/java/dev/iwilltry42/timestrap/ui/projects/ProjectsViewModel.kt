e5f7c29c01a04ce0e1c3e9574a2ea5f09668e41a
Thorsten Klein <iwilltry42@gmail.com>
2020-07-13 11:22:43 +0200

    add client name chip to projects

--- app/src/main/java/dev/iwilltry42/timestrap/ui/projects/ProjectsViewModel.kt
+++ app/src/main/java/dev/iwilltry42/timestrap/ui/projects/ProjectsViewModel.kt
@@ -1,12 +1,19 @@
 package dev.iwilltry42.timestrap.ui.projects
+import android.util.Log
 import androidx.recyclerview.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import com.android.volley.Request
+import com.google.android.material.chip.Chip
+import com.google.android.material.chip.ChipGroup
+import dev.iwilltry42.timestrap.AppPreferences
 import dev.iwilltry42.timestrap.R
 
 import dev.iwilltry42.timestrap.content.projects.ProjectContent.Project
+import dev.iwilltry42.timestrap.requestAPIObject
+import dev.iwilltry42.timestrap.requestAPIObjectWithTokenAuth
 
 /**
  * [RecyclerView.Adapter] that can display a [Project].
@@ -43,6 +50,23 @@ class ProjectRecyclerViewAdapter(
             itemView.setOnClickListener {
                 clickListener.onItemClicked(project)
             }
+
+            val path = "/" + project.client.split("/".toRegex(),  4)[3]
+            Log.i("Path", path)
+            requestAPIObjectWithTokenAuth(itemView.context, AppPreferences.address, path, AppPreferences.token) {
+                success, response ->
+                if (success && response != null) {
+                    // Display the Task rate as a chip
+                    val chipGroup = this.itemView.findViewById<ChipGroup>(R.id.item_chip_group)
+                    chipGroup.removeAllViews() // remove all chips and then re-create them
+                    val chipProjectClient = Chip(this.itemView.context)
+                    chipProjectClient.text = response["name"].toString()
+                    chipGroup.addView(chipProjectClient)
+                    chipGroup.visibility = View.VISIBLE
+                }
+            }
+
+
         }
     }
 }
